a8687ade3709d1f20240113b3a8e341513eb8baa
dhopathikruthika <106800941+dhopathikruthika@users.noreply.github.com>
2022-06-13 14:12:23 +0530

    Create flip.c

---
+++ flip.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int num, flippedNumber;
+
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    flippedNumber = ~num;
+
+    printf("Original number = %d (in decimal)\n", num);
+    printf("Number after bits are flipped = %d (in decimal)", flippedNumber);
+
+    return 0;
+}
