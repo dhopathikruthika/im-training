7470a5f203a8af12d443929f6c6642968a71a4ca
dhopathikruthika <106800941+dhopathikruthika@users.noreply.github.com>
2022-07-01 10:54:21 +0530

    Update delete.c

--- delete.c
+++ delete.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct Node 
-	{
-	int data;
-	struct Node* next;
-	} Node;
+{
+   int data;
+   struct Node* next;
+} Node;
 void push(struct Node** head_ref, int new_data)
-	{
+{
 	struct Node* new_node
 		= (struct Node*)malloc(sizeof(struct Node));
 	new_node->data = new_data;
 	new_node->next = (*head_ref);
 	(*head_ref) = new_node;
-	}
-Node* deleteKey(Node* head, int x)
-	{
+}
+Node* deleteKey(Node* head, int x)	
+{
 	if (!head)
 		return head;
 	while (head && head->data == x)
 		head = head->next;
 	Node *curr = head, *prev = NULL;
 	while (curr) 
-		{
+	{
 			if (curr->data == x)
 				prev->next = curr->next;
 			else
 				prev = curr;
 			curr = curr->next;
-		}
-	return head;
 	}
+	return head;
+}
 
 void printList(Node* node)
-	{
+{
 	while (node != NULL) 
-		{
+	{
 		printf(" %d ", node->data);
 		node = node->next;
-		}
 	}
+}
 int main()
-	{
+{
 	struct Node* head = NULL;
 
 	push(&head, 7);
@@ -60,10 +60,10 @@ int main()
 	if (!head)
 		printf("\nNo element present in the Linked list\n");
 	else 
-		{
+	{
 		printf("\nLinked List after Deletion is:\n");
 		printList(head);
-		}
-	return 0;
 	}
+	return 0;
+}
 
